0960f1159c58b2da31e7fd28e8a55dab7332024a
lucywang <351328420@qq.com>
2021-05-15 10:02:25 +0800

    [update]对ab的研究

---
+++ FrameProject/FrameClient/Assets/Editor/AssetBundle/ExportAssetBundles.cs
@@ -0,0 +1,96 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+public class ExportAssetBundles : EditorWindow
+{
+    //[MenuItem("Tool/Create AssetBundle All")]
+    //private static void CreateAssetBundle()
+    //{
+    //    Debug.Log("Create AssetBundle...");
+    //    BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath + "/assetbundle", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    //    AssetDatabase.Refresh();
+    //}
+
+    [MenuItem("BuildBundle/Build One Selected Obj Bundle/Android")]
+    //设为静态方法
+    static void Build1SelectedBundleAN()
+    {
+        //备打文件集合，当前为单个
+        AssetBundleBuild[] abb = new AssetBundleBuild[1];
+        //文件路径（当前为单个，多个文件使用数组的形式）
+        string[] path = new string[1];
+        //获取文件路径（得到鼠标选中的文件）；
+        path[0] = AssetDatabase.GetAssetPath(Selection.activeObject);
+        //备打文件路径名，从Asset/开始
+        abb[0].assetNames = path;
+        //包名
+        abb[0].assetBundleName = "assetbundle";
+        //打包（“输出路径”，备打文件集合，打包设置，目标平台）
+        BuildPipeline.BuildAssetBundles("Assets/ABs", abb, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+
+
+    }
+
+
+    [MenuItem("Tool/Create AssetBunldes")]
+    [System.Obsolete]
+    private static void CreateAssetBunldesMain()
+    {
+        //获取在Project视图中选择的所有游戏对象  
+        Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        //遍历所有的游戏对象  
+        foreach (Object obj in SelectedAsset)
+        {
+            string sourcePath = AssetDatabase.GetAssetPath(obj);
+            string filename = GetFileName(sourcePath);
+            string parentname = GetParentName(sourcePath);
+
+            //Debug.Log(obj.name + " : " + sourcePath);
+
+            if (!string.IsNullOrEmpty(filename))
+            {
+                Debug.Log("打包 " + obj.name + " : " + sourcePath);
+
+                AssetImporter ai = AssetImporter.GetAtPath(sourcePath);
+                if (ai.assetBundleName != parentname + ".assetbundle")
+                {
+                    ai.assetBundleName = parentname + ".assetbundle";
+                }
+            }
+        }
+
+        string path = Application.streamingAssetsPath + "/assetbundle";
+        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath + "/assetbundle", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+
+        //刷新编辑器  
+        AssetDatabase.Refresh();
+    }
+    private static string GetParentName(string path)
+    {
+        string[] strs = path.Split('/');
+        //string parent = strs[strs.Length - 2];
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (strs[i].Contains("_ab")) return strs[i];
+        }
+        return "";
+    }
+
+    private static string GetFileName(string path)
+    {
+        string[] strs = path.Split('/');
+        string filename = strs[strs.Length - 1];
+
+        if (filename.Contains("."))
+        {
+            return filename;
+        }
+        else
+        {
+            return "";
+
+        }
+    }
+}
